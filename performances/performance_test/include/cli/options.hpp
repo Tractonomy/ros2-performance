36a39cbb5d9f24908f29498027393f4d77b638ae
Elias De Coninck <elias@tractonomy.com>
2020-10-28 20:30:14 +0100

    do not set defaults twice
    defaults set in tracking_options struct

--- performances/performance_test/include/cli/options.hpp
+++ performances/performance_test/include/cli/options.hpp
@@ -28,11 +28,6 @@ public:
         name_threads = true;
         duration_sec = 5;
         resources_sampling_per_ms = 500;
-        tracking_options.is_enabled = true;
-        tracking_options.late_percentage = 20;
-        tracking_options.late_absolute_us = 5000;
-        tracking_options.too_late_percentage = 100;
-        tracking_options.too_late_absolute_us = 50000;
     };
 
     Options(int argc, char** argv)
